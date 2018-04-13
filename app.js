e62aecaffcca6b73b472a2de39e78e5165828026
Nathan Antebi <33539881+NathanAntebi@users.noreply.github.com>
2018-04-13 12:40:50 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,329 @@
+
+//BUDGET CONTROLLER
+var budgetController = (function(){
+   var Expense = function(id, description, value){
+       this.id = id;
+       this.description = description;
+       this.value = value;
+   };
+                         
+    var Income = function(id, description, value){
+       this.id = id;
+       this.description = description;
+       this.value = value;
+   };
+    
+    var calculateTotal = function(type){
+        var sum = 0;
+        data.allItems[type].forEach(function(cur)
+        {
+            sum = sum+cur.value;
+            
+        });
+        data.totals[type] = sum;
+        
+    };
+    
+    
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        
+        totals: {
+            exp: 0,
+            inc: 0  
+        }, 
+        
+        budget: 0
+    };
+    
+    return {
+        addItem: function(type, des, val){
+            var newItem, id;
+            
+            
+            //creating new ID
+            if (data.allItems[type].length>0){
+                id =  data.allItems[type][data.allItems[type].length -1].id + 1;
+            }
+            else{
+                id =0;
+            }
+            
+            
+            //creating new item based on inc or exp
+            if(type==='inc'){
+                newItem = new Income(id, des, val);
+            }
+            else if(type==='exp'){
+                newItem = new Expense(id, des, val);
+            }
+            
+            
+            //add item to corresponding array
+            data.allItems[type].push(newItem);
+            
+            return newItem;
+        },     
+        calculateBudget: function(){
+          
+            // calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+            
+            //calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+            
+            
+        },   
+        getBudget: function(){
+            return{
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+            }
+        },
+    
+        test: function(){
+            console.log(data);
+        }
+    };
+    
+})();
+
+//UI CONTROLLER
+var UIController = (function(){
+    
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        incomeContainer: '.income__list',
+        expenseContainer: '.expenses__list',
+        addButton: '.add__btn',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container',
+        expensesPercLabel:'.item__percentage',
+        dateLabel: '.budget__title--month'
+    };
+    
+    var formatNumber=function(num, type){
+                var numSplit, int, dec, sign;
+              num = Math.abs(num);
+              num = num.toFixed(2);
+                
+                numSplit = num.split('.');
+                
+                int = numSplit[0];
+                
+                if(int.length > 3){
+                   int = int.substr(0,int.length - 3) + ',' + int.substr(int.length - 3,3);
+                }
+                
+                
+                
+                
+                dec = numSplit[1];
+                
+                if(type==='exp'){
+                    sign = '-';
+                }
+                else{
+                    sign = '+';
+                }
+                
+                return sign + ' ' + int +'.'+ dec;
+                
+                
+                
+            };
+    
+    var nodeListForEach = function(list, callback){
+                    for(var i =0; i< list.length; i++){
+                       callback(list[i], i); 
+                    }
+                };
+    
+    return{
+        getInput: function(){
+            return{
+                type: document.querySelector(DOMstrings.inputType).value, //will be either inc or exp
+            
+                description:
+                document.querySelector(DOMstrings.inputDescription).value,
+                // will get value of description
+            
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
+                //will get the value inputed    
+            };
+            
+        }, 
+        
+            changedType: function(){
+                var fields = document.querySelectorAll(
+                        DOMstrings.inputType + ','+
+                        DOMstrings.inputDescription + ',' +
+                        DOMstrings.inputValue);
+                
+                nodeListForEach(fields, function(cur) {
+                   cur.classList.toggle('red-focus') ;
+                });
+                
+                document.querySelector(DOMstrings.addButton).classList.toggle('red');
+            },
+        
+        
+           getDOMStrings: function(){
+                return DOMstrings;
+            },
+        
+        
+        displayMonth: function(){
+            
+            var now = new Date();
+            
+            var year = now.getFullYear();
+            
+            var months = ['January', 'February', 'March', 'April', 'May', 'June',
+                         'July', 'August', 'September', 'October', 'November', 'December'];
+            
+            var month = now.getMonth();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+            
+        },
+        
+        
+         
+
+        
+        addListItem: function(obj,type){
+            var html, newHTML, element;
+            //Create HTML string w/ placeholder text
+            
+            if(type === 'inc'){
+               element = DOMstrings.incomeContainer;
+                
+                
+               html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div><div class="right clearfix"> <div class="item__value">%value%</div><div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'; 
+            }
+            else if(type === 'exp'){
+                element = DOMstrings.expenseContainer;
+                
+                
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">0%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            
+            }
+            
+            
+             
+            //replace placeholder text with actual data
+            newHTML = html.replace('%id', obj.id);
+            newHTML = newHTML.replace('%description%', obj.description);
+            newHTML = newHTML.replace('%value%', formatNumber(obj.value, type));
+            
+            //insert html into dom
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHTML);
+            
+            
+        },
+        
+        deleteListItem: function(selectorID){
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
+            
+        },
+        
+        
+        clearFields: function(){
+           var fields, fieldsArr;
+            //queySelecterAll returns a list
+           fields = document.querySelectorAll('.add__description'+ ', ' + '.add__value');
+            
+           //transform list into array
+           fieldsArr = Array.prototype.slice.call(fields);
+            
+            fieldsArr.forEach(function(current, index, array){
+                current.value = "";
+            });
+            
+            fieldsArr[0].focus();      
+        },
+        
+        displayBudget: function(obj){
+            
+            obj.budget > 0 ? type = 'inc' : type ='exp'
+            document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
+             document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc,'inc');
+             document.querySelector(DOMstrings.expensesLabel).textContent =formatNumber(obj.totalExp, 'exp');
+        }
+        
+    };   
+})();
+//GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl){
+    
+    var DOM = UICtrl.getDOMStrings();
+    var setUpEventListeners = function(){
+        
+          document.querySelector(DOM.addButton).addEventListener('click', ctrlAddItem);
+          document.addEventListener('keypress', function(event){
+              if(event.keyCode===13 || event.which===13){
+                ctrlAddItem();
+            }
+        
+          });
+        
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
+        
+        
+    }; 
+    var updateBudget = function(){
+        // 1. calculate the budget
+        budgetCtrl.calculateBudget();
+        
+        // 2. return the budget
+        var budget = budgetCtrl.getBudget();
+        
+        // 3. display budget on uI
+        UICtrl.displayBudget(budget);
+    };
+    
+    var ctrlAddItem = function(){
+        var input, newItem;
+        // 1. get input data
+        input = UICtrl.getInput();
+        
+        if(input.description !== "" && !isNaN(input.value) && input.value>0){
+            // 2. add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
+            // 3. add item to UI
+            UICtrl.addListItem(newItem, input.type);
+
+            //4. clear fields
+            UICtrl.clearFields();
+
+            //. 5 calculate and update budget
+            updateBudget();
+        }
+    };
+    return{
+        init: function(){
+            setUpEventListeners();
+            UICtrl.displayMonth();
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: 0
+            });
+        }
+    }
+})(budgetController, UIController);
+
+controller.init();
